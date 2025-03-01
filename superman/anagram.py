5791eb2d3fdd7b3b24431de18f6a716d5c2c99d9
ssah <shubham_sah@intuit.com>
2025-03-01 01:52:59 -0800

    python update

--- superman/anagram.py
+++ superman/anagram.py
@@ -4,12 +4,12 @@
 def check(s1, s2):
      
     # the sorted strings are checked
-    if(sorted(s1)== sorted(s2)):
+    if(sorted(s1) == sorted(s2)):
         print("The strings are anagrams.")
     else:
         print("The strings aren't anagrams.")        
          
 # driver code 
-s1 ="listen"
+s1 ="shubham"
 s2 ="silent"
 check(s1, s2)
