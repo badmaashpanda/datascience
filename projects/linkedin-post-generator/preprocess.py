c5ee01389d7fe1ffa3f49a693254fb9ee9c71d33
ssah <shubham_sah@intuit.com>
2025-03-15 13:34:23 -0700

    projets

--- projects/linkedin-post-generator/preprocess.py
+++ projects/linkedin-post-generator/preprocess.py
@@ -1,24 +1,25 @@
 import json
 
 def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"):
+    enriched_posts = []
     with open(raw_file_path, encoding='utf-8') as file:
         posts = json.load(file)
-        print(posts)
-#         for post in posts:
-#             extract_metadata(post['text'])
-#             post_with_metadata = post | metadata
-#             enriched.posts.append(post_with_metadata)
+        #print(posts)
+        for post in posts:
+            metadata = extract_metadata(post['text'])
+            post_with_metadata = post | metadata
+            print(post_with_metadata)
+            enriched_posts.append(post_with_metadata)
 
-#             post = {'text':'abc' , 'engagement' : 500}'
-#             metadata = {'line_count':10, 'language' : 'English', 'tags': ['Mental Health','Motivation']}
+    for epost in enriched_posts:
+        print(epost)
 
-# def extract_metadata(post):
-#     return {
-#         'line_count':10,
-#         'language' : 'English',
-#         'tags': ['Mental Health','Motivation']
-
-#     }
+def extract_metadata(post):
+    return {
+        'line_count':10,
+        'language' : 'English',
+        'tags': ['Mental Health','Motivation']
+    }
 
 if __name__ == "__main__":
     process_posts("data/raw_posts.json","data/processed_posts.json")
